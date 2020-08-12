b6f10a3dad0994f421326f7dd662ce50e5c5f5c9
Le Duy Tung <tungld@i.hivetech.vn>
2020-08-12 14:10:03 +0700

    Union Types

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: 'Tung',
-    age: 26,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-var favoriteActivities;
-favoriteActivities = ['Sports', 'Cooking'];
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(n1, n2) {
+    var result;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        result = n1 + n2;
+    }
+    else {
+        result = n1.toString() + n2.toString();
+    }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('Is admin');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("Duy", "Tung");
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,25 +1,15 @@
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 5, READ_ONLY = 100, AUTHOR = 'AUTHOR'
-};
-
-const person= {
-  name: 'Tung',
-  age: 26,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN
+function combine(n1: number | string, n2: number | string) {
+  let result;
+  if(typeof n1 === 'number' && typeof n2 === 'number') {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  }
+  return result;
 }
 
-let favoriteActivities: string[];
-favoriteActivities = ['Sports', 0];
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if(person.role === Role.ADMIN) {
-  console.log('Is admin');
-}
+const combinedNames = combine("Duy", "Tung");
+console.log(combinedNames);
