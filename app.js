f7a539725a7b5bb6dc0c98e508a78afb7ae6894e
Le Duy Tung <tungld@i.hivetech.vn>
2020-08-13 10:10:17 +0700

    Literal Types

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function combine(n1, n2) {
+function combine(n1, n2, resultConversion) {
     var result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2;
+    if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+        result = +n1 + +n2;
     }
     else {
         result = n1.toString() + n2.toString();
     }
     return result;
+    // if(resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine("Duy", "Tung");
+var combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine("Duy", "Tung", 'as-text');
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
-function combine(n1: number | string, n2: number | string) {
+function combine(n1: number | string, n2: number | string, resultConversion: 'as-number' | 'as-text') {
   let result;
-  if(typeof n1 === 'number' && typeof n2 === 'number') {
-    result = n1 + n2;
+  if(typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
   return result;
+  // if(resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine("Duy", "Tung");
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+
+const combinedNames = combine("Duy", "Tung", 'as-text');
 console.log(combinedNames);
