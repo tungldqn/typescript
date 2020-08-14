82d86cf1ed11616fd581c21e2a966c82f360b52c
Le Duy Tung <tungld@i.hivetech.vn>
2020-08-14 16:19:13 +0700

    The never Type

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occurred', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -3,3 +3,9 @@ let userName: string;
 
 userInput = 5;
 userInput = 'Max';
+
+function generateError(message: string, code: number): never {
+  throw {message: message, errorCode: code}
+}
+
+generateError('An error occurred', 500);
