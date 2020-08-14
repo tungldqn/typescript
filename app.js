ed837fb221e9ed1b92c638170c5afc5d0163e130
Le Duy Tung <tungld@i.hivetech.vn>
2020-08-14 14:13:03 +0700

    Functions as Types

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function add(n1, n2) {
 }
 function printResult(num) {
     console.log('Result: ' + num);
-    return;
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
