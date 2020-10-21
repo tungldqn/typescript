df75f76ef44a7688b79d47536a97e8c8443e3fa7
Le Duy Tung <tungld@i.hivetech.vn>
2020-10-21 14:06:06 +0700

    readonly properties

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ class Department {
   // private name: string;
   private employees: string[] = [];
 
-  constructor(private id: string, public name: string) {
+  constructor(private readonly id: string, public name: string) {
     
   }
 
