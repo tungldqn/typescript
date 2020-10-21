514c28b6c4f908117fe9825f398bf55b64e0f2bf
Le Duy Tung <tungld@i.hivetech.vn>
2020-10-21 17:21:57 +0700

    inheritance

--- src/app.ts
+++ src/app.ts
@@ -31,9 +31,39 @@ class Department {
   }
 }
 
-const  account = new Department('a1', 'Accounting');
+const account = new Department('a1', 'Accounting');
 
-account.addEmployee('Tung');
-// account.employees[1] = 'Test';
-account.printEmployeeInformation();
+// account.addEmployee('Tung');
+// // account.employees[1] = 'Test';
+// account.printEmployeeInformation();
 account.describe();
+
+class ItDepartment extends Department {
+
+  constructor(id: string, name: string, private admins: string[]) {
+    super(id, name);
+  }
+}
+
+const ItAccount = new ItDepartment('a2', 'it', ['tung']);
+ItAccount.describe();
+console.log(ItAccount);
+
+class AccountingDepartment extends Department {
+  constructor(id: string, name: string, private reports: string[]) {
+    super(id, name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new AccountingDepartment('a3', 'account', ['report1']);
+accounting.addReport('report2')
+accounting.printReports();
+console.log(accounting);
