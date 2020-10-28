e10b4d5694b47d584de062180edd24d483bb45eb
Le Duy Tung <tungld@i.hivetech.vn>
2020-10-28 18:20:23 +0700

    optional parameters and properties

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ add = (n1: number, n2: number) => {
 }
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -17,20 +18,27 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    if(this.name) {
+      console.log(phrase + ' ' + this.name);
+    } else {
+      console.log('hi');
+      
+    }
   }
 }
 
 let user1: Greetable;
-user1 = new Person('Tung');
+user1 = new Person();
 user1.greet('Hi there - I am');
 
 console.log(user1);
