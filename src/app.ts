8d01b0fea32583e866cbbb624e8a953ada4e49c0
Le Duy Tung <tungld@i.hivetech.vn>
2020-10-28 18:14:07 +0700

    Interfaces as Function Types

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+}
+
 interface Named {
   readonly name: string;
 }
