912f2cfa0d2c652b5a0032c7ab4e0d037a9f54d0
Le Duy Tung <tungld@i.hivetech.vn>
2020-11-05 17:17:12 +0700

    creating a generic function

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,10 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 });
 
 promise.then(data => data.split(' '));
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({ name: 'Max'}, { age: 30 });
+console.log(mergeObj.name);
