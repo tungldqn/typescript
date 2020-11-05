84a5155c7657060f8ed05c6e86d2ac279c1390c1
Le Duy Tung <tungld@i.hivetech.vn>
2020-11-05 17:24:08 +0700

    working with constrains

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,9 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 
 promise.then(data => data.split(' '));
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergeObj = merge({ name: 'Max'}, { age: 30 });
-console.log(mergeObj.name);
+const mergeObj = merge({ name: 'Max'}, { age: 30});
+console.log(mergeObj);
