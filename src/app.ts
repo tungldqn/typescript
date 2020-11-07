919fffdb6f48f454d4456d60bd1fedd2cd72e873
tungldqn <tungldqn@gmail.com>
2020-11-07 10:40:40 +0700

    another generic function

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,26 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 
 promise.then(data => data.split(' '));
 
-function merge<T extends object, U extends object>(objA: T, objB: U) {
+function merge<T extends object, U>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const mergeObj = merge({ name: 'Max'}, { age: 30});
-console.log(mergeObj);
+console.log(mergeObj.name);
+
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there'));
+console.log(countAndDescribe(['Cook', 'Play']));
