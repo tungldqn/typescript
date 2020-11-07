b8dcaa8f3130fe8ad2be959e3d93f39631174f0a
tungldqn <tungldqn@gmail.com>
2020-11-07 17:54:17 +0700

    generic class

--- src/app.ts
+++ src/app.ts
@@ -38,3 +38,35 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({ name: 'Tung' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if(this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item, 1));
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Tung');
+textStorage.addItem('Toan');
+textStorage.removeItem('Toan');
+console.log(textStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// const tungObj = { name: 'Tung' }
+// objStorage.addItem(tungObj);
+// objStorage.addItem({ name: 'Min' });
+// objStorage.removeItem(tungObj);
+// console.log(objStorage.getItems());
